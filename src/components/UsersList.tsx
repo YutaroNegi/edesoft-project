bfbce7f703e1e7de33d104ff187202bb5cca2299
Yutaro Negi <yutaro.sousa@liveuniversity.com>
2022-08-25 10:49:05 -0300

    first loading added

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -1,5 +1,7 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import List from '@mui/material/List';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box'
 
 import UserListItem from "./UserListItem";
 import AddUser from './AddUser'
@@ -8,6 +10,7 @@ import { useAppSelector, useAppDispatch } from '../redux/hooks'
 import { setAllUsers } from '../redux/userSlice'
 
 function UserList(){
+    const [loading, setLoading] = useState<boolean>(false)
     const users = useAppSelector(state => state.user.value)
     const dispatch = useAppDispatch()
 
@@ -16,27 +19,32 @@ function UserList(){
     }, []);
 
     async function fetchAllUsers() {
+        setLoading(true)
         const response = await fetch('https://fakestoreapi.com/users');
         const data = await response.json()
     
         dispatch(setAllUsers(data))
-
-        setTimeout(() => {
-            console.log(users);
-        }, 1000);
+        setLoading(false)
     }
 
     return(
         <>
-            <AddUser/>
+            { !loading && (<AddUser/>)}
+            
 
             <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper', display: 'block', margin: '0.6em auto auto auto'}}>
+
+            { loading && (<Box sx={{ display: 'grid', placeItems: 'center'}}><CircularProgress /></Box>)}
+            
+            
                 {users.map((user)=>{
                     return(
                         <UserListItem key={user.id} user={user}/>
                     )
                 })}
+
             </List>
+
         </>
     )
 }
